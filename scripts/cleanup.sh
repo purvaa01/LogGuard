dd9f68086e78a7e9cb3fad05dd3074d2a1c742b8
Ubuntu <ubuntu@ip-172-31-38-80.eu-north-1.compute.internal>
2026-05-05 07:06:21 +0000

    Added compression and archive directory

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 log_dir="/home/ubuntu/logguard/logs"
+archive_dir="/home/ubuntu/logguard/archive"
 
-find $log_dir -type f -name "*.log" -mtime +7 -delete
+mkdir -p $archive_dir
+
+# compress logs older than 7 days
+find $log_dir -type f -name "*.log" -mtime +7 -exec gzip {} \;
+
+# move zips logs to archive
+find $log_dir -type f -name "*.gz" -exec mv {} $archive_dir \;
+
 
