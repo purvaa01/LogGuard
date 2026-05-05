133394d884668f81b6de38c3267c406a24ca8cad
Ubuntu <ubuntu@ip-172-31-38-80.eu-north-1.compute.internal>
2026-05-05 07:14:42 +0000

    Added archive storage

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -5,10 +5,12 @@ archive_dir="/home/ubuntu/logguard/archive"
 
 mkdir -p $archive_dir
 
+echo "Starting log cleanup"
+
 # compress logs older than 7 days
 find $log_dir -type f -name "*.log" -mtime +7 -exec gzip {} \;
 
 # move zips logs to archive
 find $log_dir -type f -name "*.gz" -exec mv {} $archive_dir \;
 
-
+echo "Cleanup Completed."
