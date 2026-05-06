8be3d422a9d8daa9206cb0504feeacf172c7b544
Ubuntu <ubuntu@ip-172-31-38-80.eu-north-1.compute.internal>
2026-05-06 09:10:22 +0000

    automated log archival to AWS S3

--- scripts/archive_logs.sh
+++ scripts/archive_logs.sh
@@ -3,6 +3,7 @@
 
 log_dir="/home/ubuntu/logguard/logs"
 archive_dir="/home/ubuntu/logguard/archive"
+temp_dir="/home/ubuntu/logguard/temp_logs"
 s3_bucket="s3://logguard-archive-purva"
 
 timestamp=$(date +%Y-%m-%d_%H-%M-%S)
@@ -11,23 +12,39 @@ archive_name="logs_$timestamp.tar.gz"
 
 echo "Starting log archival"
 
-#create zip archive
-tar -czf $archive_dir/$archive_name $log_dir/*.log
+mkdir -p $temp_dir
 
-echo "Archive created: $archive_name"
+#find logs older than 7 days 
+find $log_dir -name "*.log" -mtime +7 -exec mv {} $temp_dir/ \;
 
-#upload to s3
-aws s3 cp $archive_dir/$archive_name $s3_bucket/
+if [ "$(find $temp_dir -type f)" ]; then
+       echo "old logs found. Creating archive"
 
-if [ $? -eq 0 ]; then
-	echo "upload successful"
 
-	#remove local archive after successful upload on s3
-	rm -f $archive_dir/$archive_name
+ 	#create zip archive
+	tar -czf $archive_dir/$archive_name -C $temp_dir
 
-	echo "Local archive removed"
+	echo "Archive created: $archive_name"
 
+	#upload to s3
+	aws s3 cp $archive_dir/$archive_name $s3_bucket/
+
+	if [ $? -eq 0 ]; then
+		echo "upload successful"
+
+		#remove local archive after successful upload on s3
+		rm -f $archive_dir/$archive_name
+
+		rm -rf $temp_dir/*
+
+		echo "Local cleanup completed."
+
+	else
+		echo "Upload Failed"
+	fi
 else
-	echo "Upload Failed"
+	echo "No logs are older than 7 days."
 fi
+
+
 
